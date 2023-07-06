9ee5890a04ef6be35e3ad87ca21bb87e2dc9d215
Scythe Prince v9 <ScythePrincev9@gmail.com>
2023-07-06 12:28:55 +0300

    Day 3

--- Main_script.js
+++ Main_script.js
@@ -1,7 +1,22 @@
-const electro_button =document.getElementById("link1");
-const electro_menu = document.getElementById("electro-menu");
-electro_button.onclick =function(e) {
-    electro_menu.classList.toggle("active");
-    electro_menu.classList.toggle("hidden");
+const show_menu = document.querySelector("#show-menu");
+const sec_nav_bar = document.querySelector(".sec-nav");
+
+// const category = document.querySelector("#special-link");
+// const category_menu = document.querySelector("#categories");
+
+show_menu.addEventListener("click",function (){
+    sec_nav_bar.classList.toggle("hidden");
+})
+
+// category.addEventListener("click",function(){
+//     category_menu.classList.toggle("active");
+// category_menu.classList.toggle("hidden");
+// })
+
+// const electro_button =document.getElementById("link1");
+// const electro_menu = document.getElementById("electro-menu");
+// electro_button.onclick =function(e) {
+//     electro_menu.classList.toggle("active");
+//     electro_menu.classList.toggle("hidden");
     
-}
+// }
