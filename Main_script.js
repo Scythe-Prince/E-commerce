40a151609b985762d70b55035a5387671d2ae3ea
Scythe Prince v9 <ScythePrincev9@gmail.com>
2023-07-12 19:37:58 +0300

    day 5

--- Main_script.js
+++ Main_script.js
@@ -21,66 +21,153 @@ const Stationery_menu = document.querySelector("#Stationery-menu");
 const Music_menu = document.querySelector("#Music-menu");
 
 // #################################################################
-const link1=document.querySelector("#link1");
-const link2=document.querySelector("#link2");
-const link3=document.querySelector("#link3");
-const link4=document.querySelector("#link4");
-const link5=document.querySelector("#link5");
-const link6=document.querySelector("#link6");
-const link7=document.querySelector("#link7");
-const link8=document.querySelector("#link8");
-const link9=document.querySelector("#link9");
-const link10=document.querySelector("#link10");
-const link11=document.querySelector("#link11");
-const link12=document.querySelector("#link12");
+const arr_menu = [
+    electro_menu,
+    mobile_menu,
+    grocery_menu,
+    beauty_menu,
+    fashion_menu,
+    home_menu,
+    Sports_menu,
+    Baby_menu,
+    Automotive_menu,
+    Books_menu,
+    Stationery_menu,
+    Music_menu,
+];
+
+const link1 = document.querySelector("#link1");
+const link2 = document.querySelector("#link2");
+const link3 = document.querySelector("#link3");
+const link4 = document.querySelector("#link4");
+const link5 = document.querySelector("#link5");
+const link6 = document.querySelector("#link6");
+const link7 = document.querySelector("#link7");
+const link8 = document.querySelector("#link8");
+const link9 = document.querySelector("#link9");
+const link10 = document.querySelector("#link10");
+const link11 = document.querySelector("#link11");
+const link12 = document.querySelector("#link12");
 // const category = document.querySelector("#special-link");
 // const category_menu = document.querySelector("#categories");
-link1.addEventListener("click",function(){
+link1.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+            
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     electro_menu.classList.toggle("hidden");
-})
+});
 
-link2.addEventListener("click",function(){
+link2.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     mobile_menu.classList.toggle("hidden");
-})
+});
 
-link3.addEventListener("click",function(){
+link3.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     grocery_menu.classList.toggle("hidden");
-})
+});
 // ###############################################################
-link4.addEventListener("click",function(){
+link4.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     beauty_menu.classList.toggle("hidden");
-})
-link5.addEventListener("click",function(){
+});
+link5.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     fashion_menu.classList.toggle("hidden");
-})
-link6.addEventListener("click",function(){
+});
+link6.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     home_menu.classList.toggle("hidden");
-})
+});
 // ###############################################################
-link7.addEventListener("click",function(){
+link7.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     Sports_menu.classList.toggle("hidden");
-})
-link8.addEventListener("click",function(){
+});
+link8.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     Baby_menu.classList.toggle("hidden");
-})
-link9.addEventListener("click",function(){
+});
+link9.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     Automotive_menu.classList.toggle("hidden");
-})
+});
 // ###############################################################
-link10.addEventListener("click",function(){
+link10.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     Books_menu.classList.toggle("hidden");
-})
-link11.addEventListener("click",function(){
+});
+link11.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     Stationery_menu.classList.toggle("hidden");
-})
-link12.addEventListener("click",function(){
+});
+link12.addEventListener("click", function () {
+    for (let i = 0; i < arr_menu.length; i++) {
+        if (arr_menu[i].classList.contains("hidden") === true) {
+
+        } else if (arr_menu[i].classList.contains("hidden") === false)
+            arr_menu[i].classList.add("hidden");
+    }
     Music_menu.classList.toggle("hidden");
-})
+});
 // ###############################################################
 
-show_menu.addEventListener("click",function (){
+show_menu.addEventListener("click", function () {
     sec_nav_bar.classList.toggle("hidden");
-})
+});
 
 // category.addEventListener("click",function(){
 //     category_menu.classList.toggle("active");
@@ -92,5 +179,5 @@ show_menu.addEventListener("click",function (){
 // electro_button.onclick =function(e) {
 //     electro_menu.classList.toggle("active");
 //     electro_menu.classList.toggle("hidden");
-    
+
 // }
